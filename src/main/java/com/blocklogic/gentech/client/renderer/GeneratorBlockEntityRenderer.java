857a7e2da35f3abb29c1df518c6c8b99130f2935
deonjonker123 <deon.jonker@hotmail.com>
2025-06-17 03:22:03 +0200

    Refined the blocks

--- src/main/java/com/blocklogic/gentech/client/renderer/GeneratorBlockEntityRenderer.java
+++ src/main/java/com/blocklogic/gentech/client/renderer/GeneratorBlockEntityRenderer.java
@@ -14,6 +14,7 @@ import net.minecraft.world.level.block.state.BlockState;
 public class GeneratorBlockEntityRenderer implements BlockEntityRenderer<GeneratorBlockEntity> {
 
     public GeneratorBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
+
     }
 
     @Override
@@ -32,12 +33,21 @@ public class GeneratorBlockEntityRenderer implements BlockEntityRenderer<Generat
 
         poseStack.translate(0.5, 0.5, 0.5);
 
-        float scale = 0.7f;
+        float scale = 0.5f;
         poseStack.scale(scale, scale, scale);
 
         long gameTime = level.getGameTime();
         float rotation = (gameTime + partialTick) * 2.0f;
         poseStack.mulPose(Axis.YP.rotationDegrees(rotation));
+
+        float progress = blockEntity.getProgressLevel();
+        if (progress > 0) {
+            float bobOffset = (float) Math.sin((gameTime + partialTick) * 0.2) * 0.05f;
+            poseStack.translate(0, bobOffset, 0);
+
+            float progressScale = 0.8f + (progress * 0.4f);
+            poseStack.scale(progressScale, progressScale, progressScale);
+        }
 
         poseStack.translate(-0.5, -0.5, -0.5);
 
